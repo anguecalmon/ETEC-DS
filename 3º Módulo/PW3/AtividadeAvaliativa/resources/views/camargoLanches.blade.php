b509c0c814dcda89b5355d3b0c89e30b50f37c8a
anguecalmon <calmon.principal@gmail.com>
2023-03-16 22:22:18 -0300

    Adiciona a atividade avaliativa de laravel

---
+++ 3º Módulo/PW3/AtividadeAvaliativa/resources/views/camargoLanches.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Camargo Lanches</title>
+    <link rel="stylesheet" href="/css/estiloLanche.css">
+</head>
+<body>
+    <img src="https://gordaolanches.com.br/wp-content/uploads/lanche-do-gordao-cosmopolis.png" alt="Lanchão">
+
+    <h1> Esse delicioso lanche é um oferecimento Camargo Lanches! </h1>
+    <p> 😋 </p>
+</body>
+</html>
